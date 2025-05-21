6a915c1a034e01989bc8fc5eb7ae4bf64b78a14b
Guilherme Abdelnor <guigo211103@gmail.com>
2025-05-21 13:56:27 -0300

    Feat: Add custom health-check endpoint at /api/health

---
+++ backend/todo-backend/src/main/java/com/guigx7/todo/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.guigx7.todo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/api/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+}
